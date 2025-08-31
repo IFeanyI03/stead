bccc6c583e20996494817c4705714ca9d7a80582
IFeanyI03 <ifeanyiaghasili@gmail.com>
2025-08-31 17:58:57 +0100

    fix

--- src/components/commons/EcosystemCard.jsx
+++ src/components/commons/EcosystemCard.jsx
@@ -3,7 +3,7 @@ import { Button } from "./Button";
 
 export const EcosystemCard = ({ data }) => (
     <div className="w-full relative py-6 px-6 border-[0.2px]  flex flex-col bg-[#D9D9D9] rounded-[78px] rounded-b-[30px] gap-6 h-[789ppx] border-[#FFFFFF80]">
-        <div className="relative w-[95.76] h-[478px] flex justify-center items-center">
+        <div className="relative w-[95.76%] h-[478px] flex justify-center items-center">
             <img className="w-full h-full rounded-[54px] object-cover" src={data.image} alt="" />
             <div className="absolute w-[580px] flex gap-1 justify-center items-center h-[90px] backdrop-blur-[0.2px] ">
                 <SteadLogo /> <p className="text-white text-[32px] font-medium">| <span className="font-bold">{" " + data.category}</span></p>
